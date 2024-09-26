96f34aed94c3bbd5ed30d66984c10e993fe33d68
AbdelnaeemOsama <125660203+AbdelnaeemOsama@users.noreply.github.com>
2024-09-26 18:57:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+//______________________if ______________________
+let x = 3;
+
+if(x==4){
+    console.log('x is undefined');
+
+}else{
+    console.log ("your question is wrong")
+
+}
+if(x>1){
+    console.log("accessed");
+
+}else if (x>3){
+    console.log("user");
+
+}else if (x>=7&& x<=9){
+    console.log("admin");
+}else{
+    console.log ("not accessed");
+}
+//__________________ switch___________________
+switch(x){
+case 15>= x && x >=10:
+    console.log ('admin');
+    break;
+
+    case x>=7&&x<9:
+        console.log('user')
+        break;
+
+        default:
+        console.log ('not accesed');
+        break;
+}
+
+//___________________________for loop____________________
+if(i<=4){
+
+}
+for(let i = 0; ;i++){
+console.log (arr[i]);
+
+}
